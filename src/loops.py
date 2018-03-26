a2b465e66ceba1945cd75021bceee2660d6db950
tomek <tik.wesolowski@wp.pl>
2018-03-26 19:50:23 +0200

    basic loops

---
+++ src/loops.py
@@ -0,0 +1,35 @@
+
+
+### for loop
+# with range() function
+stri = ""
+for i in range(5):
+    stri += str(i) + " "
+print stri
+
+stri = ""
+for i in range(5,10):
+    stri += str(i) + " "
+print stri
+
+# iterating over list, dictionary, map etc 
+stri = ""
+custom_list = ['a', 'b', 'c', 'd']
+for i in custom_list:
+    stri += i + " "
+print stri
+
+# with enumerate
+for number, element in enumerate(custom_list):
+    print str(number) + " - " + str(element) 
+
+
+
+### while loop
+i = 0
+stri = ""
+while i<5:
+     stri+=str(i)
+     i+=1
+print stri
+
